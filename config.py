589ce63042ce23a87bc1c7abe14cceb78ad1445c
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-06-03 22:03:24 +0400

    Update config.py

--- config.py
+++ config.py
@@ -35,8 +35,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Support
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ITSZSHUKLA")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MASTIWITHFRIENDSXD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ht_bots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ht_bots_chat")
 
 # YT-API Get from @AmShashank on tg
 API_KEY = getenv("API_KEY", "")
