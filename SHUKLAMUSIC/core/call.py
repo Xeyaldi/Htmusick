c1d0371ac549f3360b684171bb5cbd15d8c7cdea
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-06-16 20:47:39 +0400

    Update call.py

--- SHUKLAMUSIC/core/call.py
+++ SHUKLAMUSIC/core/call.py
@@ -275,6 +275,11 @@ class Call(PyTgCalls):
         await assistant.join_group_call(
             config.LOGGER_ID,
             AudioVideoPiped(link),
+        )
+        await asyncio.sleep(0.2)
+        await assistant.leave_group_call(config.LOGGER_ID)
+        ,
+            AudioVideoPiped(link),
             stream_type=StreamType().pulse_stream,
         )
         await asyncio.sleep(0.2)
